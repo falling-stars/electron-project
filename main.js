7b26fede839401a53999afffbbf2a92e4fd0063c
liujianhua <1769908045@qq.com>
2020-04-05 17:43:08 +0800

    sd

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 
-function createWindow() {
-  // 创建浏览器窗口
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+app.allowRendererProcessReuse = true
+
+const createWindow = () => {
+  const window = new BrowserWindow({
+    width: 1200,
+    height: 800,
     webPreferences: {
       nodeIntegration: true
     }
   })
 
-  // 加载index.html文件
-  win.loadFile('index.html')
+  const template = [
+    {
+      label: app.name,
+      submenu: [
+        {
+          label: '退出',
+          click: () => app.quit()
+        }
+      ]
+    },
+    {
+      label: '编辑',
+      submenu: [{
+        label: '自定义项目',
+        click: () => console.log(12)
+      }]
+    }
+  ]
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+
+  window.loadFile('index.html')
 }
 
 app.whenReady().then(createWindow)
